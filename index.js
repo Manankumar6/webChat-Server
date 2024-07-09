f7fcef5b2e4b2559ed8413d6b22eced57ae553e1
Manankumar6 <manankumar2019@gmail.com>
2024-07-09 10:39:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require('cors');
+const http = require('http');
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 8000;
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow only your frontend origin
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    res.send("welcome to our chat application");
+});
+
+let users = {};
+console.log(users, "all users")
+// Create a new instance of socket.io and enable CORS
+const io = new Server(server, {
+    cors: corsOptions
+});
+
+io.on("connection", (socket) => {
+    console.log("New Connection", socket.id);
+
+    socket.on('joined', (user) => {
+        users[socket.id] = user;
+        socket.emit("welcome", { message: "Welcome to webChat" })
+        console.log(users, "all users")
+        // Notify all users except the one who just joined
+        socket.broadcast.emit("userJoined", { message: `${users[socket.id]} has joined` });
+    });
+    socket.on("message",({message,id})=>{
+        io.emit("sendMessage",{message,user:users[id],id})
+    })
+    socket.on('disconnect', () => {
+        if (users[socket.id]) {
+            socket.broadcast.emit("leave", { user: "Admin", message: `${users[socket.id]} has left` });
+            delete users[socket.id];
+        }
+    });
+});
+server.listen(PORT, () => {
+    console.log(`app is listening on the port http://localhost:${PORT}`);
+});
